2c0c67165c14339851d065b070828ccb79eddbfd
Tartykov Lev <lev_tartykov@mail.ru>
2021-11-26 01:55:27 +0300

    Защита 4-5 лаб

--- lab_05/queries/task_3.sql
+++ lab_05/queries/task_3.sql
@@ -12,4 +12,4 @@ create table if not exists dop_json
 select * from dop_json;
 
 insert into dop_json
-select * from json_object('{animal_id, animal_name, kind}', '{1, "Арчи", "Собака"}');
+select * from json_object('{animal_id, animal_name, kind}', '{2, "Арчи", "Собака"}');
