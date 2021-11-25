7477f01c65eee9875da384143444cfae6bdf1917
Tartykov Lev <lev_tartykov@mail.ru>
2021-11-26 01:54:08 +0300

    Поправки в лабу (рекурсивное ОТВ)

--- lab_02/queries/dop_request.sql
+++ lab_02/queries/dop_request.sql
@@ -4,14 +4,16 @@
   со средней тяжестью диагноза и выписать вид животных и их количество;
   отсортировать по количеству (уменьшение)
 */
+--INSERT INTO animals (id, animal_name, kind, age, id_vet, id_diagnosis, id_treatment)
+--VALUES(1001, 'Розочка', 'Канарейка', 1, 12, 3, 47);
 
-SELECT animals.id, animals.animal_name, animals.kind, vet.surname, diagn.degree_severity, count(animals.kind)
+SELECT animals.kind, vet.surname, diagn.degree_severity, count(animals.kind)
 FROM animals 
 	JOIN veterinarian AS vet ON animals.id_vet = vet.id_vet
 	JOIN diagnosis AS diagn ON animals.id_diagnosis = diagn.id_diagnosis
 WHERE vet.surname IN (SELECT veterinarian.surname
 								FROM veterinarian
-								WHERE veterinarian.surname = 'Андрианова')
+								WHERE veterinarian.surname = 'Авксентьева')
 	 AND diagn.degree_severity = 'Средняя'
-GROUP BY animals.id, animals.animal_name, animals.kind, vet.surname, diagn.degree_severity
-ORDER BY count DESC
+GROUP BY animals.kind, vet.surname, diagn.degree_severity
+ORDER BY count(animals.kind) DESC
